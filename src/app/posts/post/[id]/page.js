dd979f73d1236fb96d45fe6b4f98e73c3b687ab5
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-02-15 02:17:00 +0100

    This WORKS baby <3 !

--- src/app/posts/post/[id]/page.js
+++ src/app/posts/post/[id]/page.js
@@ -11,6 +11,7 @@ import { CommentPostBtn } from '@/app/ui/buttons/CommentPostBtn'
 import { SharePostBtn } from '@/app/ui/buttons/SharePostBtn'
 import { CommentsCount } from '@/app/ui/CommentsCount'
 import { OptionsBtn } from '@/app/ui/buttons/OptionsBtn'
+import comments_new from '@/../../mock-data/comments_new'
 
 export default async function Page({ params }) {
   const postId = params.id
@@ -18,71 +19,36 @@ export default async function Page({ params }) {
   const userId = post['user-id']
   const user = await getUser(userId)
 
-  //mock-data, will be replaced with fetch to db
-  const comments = [
-    {
-      id: 1,
-      userName: 'John Strayf',
-      body: 'This is the first comment.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit!',
-      replies: [
-        {
-          id: 4,
-          userName: 'Mikkaa Fika',
-          body: 'Reply to first comment. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          replies: [],
-        },
-      ],
-    },
-    {
-      id: 2,
-      userName: 'Dist McCoy',
-      body: 'This is the second comment with multiple replies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      replies: [
-        {
-          id: 5,
-          userName: 'Michael Brucci',
-          body: 'Reply to second comment. Vd minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod',
-          replies: [
-            {
-              id: 6,
-              userName: 'User6',
-              body: 'Reply to the first reply of second comment. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit!',
-              replies: [],
-            },
-          ],
-        },
-        {
-          id: 7,
-          userName: 'User7',
-          body: 'Another reply to second comment.',
-          replies: [],
-        },
-      ],
-    },
-    {
-      id: 3,
-      userName: 'User3',
-      body: 'This is the third comment. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur!',
-      replies: [],
-    },
+  const topCommentIDs = [
+    'c13a619f-4a38-4b6c-bd0b-9a21d5c40571',
+    '30e3a0e5-4f5d-442b-88dc-7f9993c70ac7',
+    '5e19f5b2-2c27-4b7b-a2a2-7ed7e00be64b',
   ]
 
+  const allComments = comments_new
+
   // Render comment and its replies recursively
-  function renderComments(comment, depth = 0) {
+  function renderComments(commentId, allComments, depth = 0) {
+    const comment = allComments.find((c) => c._id === commentId)
+
+    if (!comment) {
+      return null
+    }
+
     return (
       <div
         className="comment-container relative flex pt-4 px-2"
-        key={comment.id}
+        key={comment._id}
         style={{ marginLeft: depth * 20 }}
       >
         <div className="comment-styling-element comment-vertical-line absolute left-[-6px] top-14 w-3"></div>
         <div className="comment-main-content-container">
           <div className="comment-username-container relative right-6 flex items-center">
             <div className="comment-avatar w-8 h-8 bg-blue-400 rounded-md"></div>
-            <p className="comment-username ml-1 text-blue-900">{comment.userName}</p>
+            <p className="comment-username ml-1 text-blue-900">{comment.user_id}</p>
           </div>
           <div className="comment-body-container ml-4">
-            <p className="comment-body mt-1 text-lg">{comment.body}</p>
+            <p className="comment-body mt-1 text-lg">{comment.content}</p>
           </div>
           <div className="comment-btns-container flex items-center ml-4">
             <LikeBtn className="comment-btn-like" />
@@ -92,7 +58,7 @@ export default async function Page({ params }) {
             <ShareCommentBtn className="comment-btn-share" />
           </div>
           <div className="ml-[20px]">
-            {comment.replies.map((reply) => renderComments(reply, depth + 1))}
+            {comment.replies.map((replyId) => renderComments(replyId, allComments, depth + 1))}
           </div>
         </div>
       </div>
@@ -130,7 +96,7 @@ export default async function Page({ params }) {
         {/* Comments section */}
         <h3 className="comments-section-title text-lg pt-1 font-semibold">Comments:</h3>
         <div className="comments-section bg-gray-100 pl-8 pr-3 pb-6 mt-1 rounded-md">
-          {comments.map((comment) => renderComments(comment))}
+          {topCommentIDs.map((commentId) => renderComments(commentId, allComments))}
         </div>
       </div>
     </div>
