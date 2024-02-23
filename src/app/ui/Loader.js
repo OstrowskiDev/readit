e938db29beddc5f6937151624e9ec9f7fb61a499
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-02-23 18:21:48 +0100

    Cleaning up unnecessary code after loading spinner implementation.

--- src/app/ui/Loader.js
+++ src/app/ui/Loader.js
@@ -3,7 +3,7 @@ import { Spinner } from './Spinner'
 export function Loader() {
   console.log('Suspense activated!')
   return (
-    <div className="absolute flex flex-col justify-center items-center w-full h-full bg-gray-500 bg-opacity-50 z-50">
+    <div className="absolute flex flex-col justify-center items-center w-full h-full bg-gray-300 bg-opacity-50 z-20">
       <Spinner />
       <h1 className="text-white">Loading...</h1>
     </div>
