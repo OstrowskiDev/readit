9792a84f6f15c192395d4816250b4661b7fc754d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-03-15 22:38:06 +0100

    Added UserPosts page and route.

---
+++ src/app/user/[id]/posts/page.js
@@ -0,0 +1,43 @@
+import PostCard from '../ui/PostCard'
+import PostsSearch from '../ui/PostsSearch'
+import CreateBtn from '../ui/buttons/CreateBtn'
+import sanitizeHtml from 'sanitize-html'
+import { getPosts, getUsers } from '../lib/db'
+
+export default async function UserPosts({ searchParams }) {
+  const posts = await getPosts()
+  const users = await getUsers()
+  const dirtyQuery = searchParams?.query || ''
+  const query = sanitizeHtml(dirtyQuery, { allowedTags: null })
+  const matchingPosts = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(query.toLowerCase()) ||
+      post.content.toLowerCase().includes(query.toLowerCase()),
+  )
+  return (
+    <div className="container mx-auto mt-8 px-4">
+      <div className="flex md:items-center flex-col md:flex-row md:h-10 mb-4">
+        <h1 className="grow below-md:hidden text-2xl font-semibold mr-4">
+          Posts
+        </h1>
+        <PostsSearch />
+        <CreateBtn />
+      </div>
+      <div className="grid grid-cols-1 2col:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-6">
+        {matchingPosts.map((post) => (
+          <PostCard
+            key={post._id}
+            _id={post._id}
+            title={post.title}
+            user_id={post.user_id}
+            user_name={
+              users.find((user) => user._id === post.user_id)?.name ||
+              'Unknown User'
+            }
+            content={post.content}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
