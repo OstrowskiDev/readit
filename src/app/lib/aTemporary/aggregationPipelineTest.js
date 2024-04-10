7a002113a5130c38353cf1673b1a9780e7497407
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-04-10 23:45:06 +0200

    Aggregation for arraging posts by replies count.

--- src/app/lib/aTemporary/aggregationPipelineTest.js
+++ src/app/lib/aTemporary/aggregationPipelineTest.js
@@ -58,3 +58,34 @@ Posts.aggregate([
     },
   },
 ])
+
+//aggregation for
+Posts.aggregate([
+  {
+    $lookup: {
+      from: 'Comments',
+      localField: '_id',
+      foreignField: 'postId',
+      as: 'comments',
+    },
+  },
+  {
+    $unwind: '$comments',
+  },
+  {
+    $lookup: {
+      from: 'Comments',
+      localField: 'comments.replies',
+      foreignField: '_id',
+      as: 'replies',
+    },
+  },
+  {
+    $group: {
+      _id: '$_id',
+      totalComments: { $sum: 1 }, // Count comments for each post
+      totalReplies: { $sum: { $size: '$replies' } }, // Count replies for each post
+    },
+  },
+  // Optionally, add more stages for sorting, filtering, or projecting the results
+])
