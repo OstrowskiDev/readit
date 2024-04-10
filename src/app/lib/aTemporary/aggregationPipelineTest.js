1918939638c60672ef21cb24a0a7e9761c24d807
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-04-10 23:16:32 +0200

    Second version of Posts.aggregate

--- src/app/lib/aTemporary/aggregationPipelineTest.js
+++ src/app/lib/aTemporary/aggregationPipelineTest.js
@@ -23,3 +23,38 @@ Posts.aggregate([
     },
   },
 ])
+
+// version two:
+Posts.aggregate([
+  {
+    $lookup: {
+      from: 'Comments',
+      localField: '_id',
+      foreignField: 'parent._id',
+      as: 'comments',
+    },
+  },
+  {
+    $graphLookup: {
+      from: 'Comments',
+      startWith: '$comments._id',
+      connectFromField: '_id',
+      connectToField: 'parent._id',
+      as: 'allComments',
+    },
+  },
+  {
+    $addFields: {
+      totalComments: {
+        $size: '$allComments',
+      },
+    },
+  },
+  {
+    $group: {
+      _id: '$_id',
+      title: { $first: '$title' }, // Assuming you want to keep the title
+      totalComments: { $sum: '$totalComments' },
+    },
+  },
+])
