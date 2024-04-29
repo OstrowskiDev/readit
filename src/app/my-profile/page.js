9345b91e9410b2e315c8ebd7b6c79ec7ba775ea4
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-04-29 02:02:30 +0200

    Added transformation effect to edit user data form in my-profile.

--- src/app/my-profile/page.js
+++ src/app/my-profile/page.js
@@ -6,11 +6,13 @@ import { signIn, useSession } from 'next-auth/react'
 import Avatar from '../lib/avatars/Avatar'
 import { countUserComments, countUserPosts } from '../lib/actions'
 import { EditIco } from '../ui/icons/EditIco'
+import { set } from 'lodash'
 
 export default function MyProfile() {
   const [userData, setUserData] = useState(null)
   const [editAbout, setEditAbout] = useState(false)
   const [editData, setEditData] = useState(false)
+  const [editDataHeight, setEditDataHeight] = useState(180)
   const { data: session } = useSession()
   if (!session) signIn()
 
@@ -32,6 +34,18 @@ export default function MyProfile() {
     }
     fetchData()
   }, [session])
+
+  function handleUserDataFormVisibility() {
+    if (editData === false) {
+      setEditDataHeight(244)
+      setTimeout(() => {
+        setEditData((prevValue) => !prevValue)
+      }, 500)
+    } else {
+      setEditDataHeight(180)
+      setEditData((prevValue) => !prevValue)
+    }
+  }
 
   return (
     <>
@@ -84,15 +98,21 @@ export default function MyProfile() {
             </div>
 
             {/* user data */}
-            <div className="profile-user-data relative border-t border-gray-200 mt-4 pt-4">
+            <div
+              className={`profile-user-data relative transition-height  border-t border-gray-200 mt-4 pt-4 `}
+              style={{ height: `${editDataHeight}px` }}
+            >
               <h3 className="profile-label-user-data text-lg font-semibold text-gray-800 mb-2">
                 Your data
               </h3>
               {editData ? (
                 <UserDataForm
+                  editData={editData}
+                  setEditData={setEditData}
+                  setEditDataHeight={setEditDataHeight}
                   userData={userData}
                   setUserData={setUserData}
-                  setEditData={setEditData}
+                  handleUserDataFormVisibility={handleUserDataFormVisibility}
                 />
               ) : (
                 <ProfileUserData userData={userData} />
@@ -100,7 +120,7 @@ export default function MyProfile() {
 
               <div
                 className="user-data-edit-btn absolute top-2 right-1 p-2 w-10 h-10 hover:bg-gray-200 hover:cursor-pointer rounded-md"
-                onClick={() => setEditData((prevValue) => !prevValue)}
+                onClick={handleUserDataFormVisibility}
               >
                 <EditIco />
               </div>
@@ -168,7 +188,14 @@ function ProfileUserData({ userData }) {
   )
 }
 
-function UserDataForm({ userData, setUserData, setEditData }) {
+function UserDataForm({
+  userData,
+  setUserData,
+  editData,
+  setEditData,
+  setEditDataHeight,
+  handleUserDataFormVisibility,
+}) {
   const [formState, setFormState] = useState(userData)
 
   const handleChange = (event) => {
@@ -181,66 +208,63 @@ function UserDataForm({ userData, setUserData, setEditData }) {
   const handleSubmit = (event) => {
     event.preventDefault()
     setUserData(formState)
-    setEditData((prevValue) => !prevValue)
-    //add function to update user data in db
+    handleUserDataFormVisibility()
+    // !!!! add function to update user data in db
   }
 
   const handleCancel = () => {
-    setEditData((prevValue) => !prevValue)
+    handleUserDataFormVisibility()
   }
 
   return (
-    <form className="user-data-container">
-      <div className="user-data-name flex items-center">
-        <label className="profile-label-user-data w-20 text-md text-gray-900">
-          name:
-        </label>
+    <form className="form-user-data-container">
+      <div className="form-data-name flex items-center">
+        <label className="label-name w-20 text-md text-gray-900">name:</label>
         <input
-          className="profile-user-data w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
+          className="input-name w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
           name="name"
           value={formState.name}
           onChange={handleChange}
         />
       </div>
-      <div className="user-data-email flex items-center">
-        <label className="profile-label-user-data w-20 text-md text-gray-900">
-          email:
-        </label>
+
+      <div className="form-data-email flex items-center">
+        <label className="label-email w-20 text-md text-gray-900">email:</label>
         <input
-          className="profile-user-data w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
+          className="input-email w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
           name="email"
           value={formState.email}
           onChange={handleChange}
         />
       </div>
-      <div className="user-data-address flex items-center">
-        <label className="profile-label-user-data w-20 text-md text-gray-900">
+
+      <div className="form-data-address flex items-center">
+        <label className="label-address w-20 text-md text-gray-900">
           address:
         </label>
         <input
-          className="profile-user-data w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
+          className="input-address w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
           name="address"
           value={formState.address}
           onChange={handleChange}
         />
       </div>
-      <div className="user-data-phone flex items-center">
-        <label className="profile-label-user-data w-20 text-md text-gray-900">
-          phone:
-        </label>
+
+      <div className="form-data-phone flex items-center">
+        <label className="label-phone w-20 text-md text-gray-900">phone:</label>
         <input
-          className="profile-user-data w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
+          className="input-phone w-[330px] px-2 text-md text-gray-900 my-1 bg-gray-100 border border-gray-300 rounded-md"
           name="phone"
           value={formState.phone}
           onChange={handleChange}
         />
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-4 mb-5">
         <button
           type="button"
           onClick={handleCancel}
-          className="form-cancel-button py-[6px] px-4 mt-3 bg-gray-200 hover:bg-gray-300 text-gray-600 hover:text-gray-700 font-bold rounded ml-4"
+          className="form-cancel-button py-[6px] px-4 bg-gray-200 hover:bg-gray-300 text-gray-600 hover:text-gray-700 font-bold rounded ml-4"
         >
           Cancel
         </button>
@@ -248,7 +272,7 @@ function UserDataForm({ userData, setUserData, setEditData }) {
         <button
           type="submit"
           onClick={handleSubmit}
-          className="form-submit-button py-[6px] px-4 mt-3 ml-4 bg-blue-500 hover:bg-blue-600 text-white font-bold  rounded"
+          className="form-submit-button py-[6px] px-4 ml-4 bg-blue-500 hover:bg-blue-600 text-white font-bold  rounded"
         >
           Save changes
         </button>
