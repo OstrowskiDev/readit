887cee8bd3c8fea41c73610968ee49740903036e
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-05-18 20:08:37 +0200

    Removed routers for about and credits pages. Now their content is inside their respective posts.

---
+++ src/app/lib/aTemporary/TodoList.js
@@ -0,0 +1,24 @@
+// - add credits post and route for it
+// - add my posts route and page
+// - add favorites route and page
+// - add password client side validation:
+//     - it will work for both creating account and changing password
+//     - add account creation
+//     - chapta for account creation?
+
+// - link to github repo
+//
+// - Adding karma could be fun.
+// - Add favorites functionality: adding and removing posts to favorites.
+// - Fix responsive design for mobile devices!
+// - add validation for "about" post ID, so only user with admin role can perform CUD operations on it.
+// - add validation for passwords before they are hashed. Previous schema below:
+// password: {
+//   type: String,
+//   required: true,
+//   minlength: 8,
+//   maxlength: 128,
+//   match:
+//     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,128}$/,
+// },
+//
