db720fa315af2219fb15e9d6fc8a34acc7fbb270
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-06-08 22:43:39 +0200

    Changed structure of HTML on PostPage so it better fits new mobile and desktop layout.

--- src/app/posts/post/[id]/page.js
+++ src/app/posts/post/[id]/page.js
@@ -77,74 +77,77 @@ export default function PostPage({ params }) {
         handleMouseLeave={handleMouseLeave}
         setIsEditFormVisible={setIsEditFormVisible}
       >
-        <div className="w-full flex justify-center mt-1 md:my-8 md:px-4">
-          <div className="post-card-container relative flex flex-col justify-between max-w-[800px] w-full pt-4 px-4 md:pb-4 mx-2 rounded-md md:shadow-center-md">
-            {/* Post header */}
-            <PostHeader author={post.authorData} />
-            <div className="post-header flex justify-between mb-4">
-              <h2 className="post-title text-2xl pt-4 font-semibold">
-                {post.title}
-              </h2>
-            </div>
-
-            {/* user infobox on hover */}
-            <Suspense fallback={<UserInfoboxLoader />}>
-              {isUserHovered && (
-                <LazyUserInfobox
-                  author={post.authorData}
-                  handleMouseEnter={handleMouseEnter}
-                  handleMouseLeave={handleMouseLeave}
-                />
-              )}
-            </Suspense>
-
-            {/* Post body */}
-            <pre className="post-body-text font-sans whitespace-pre-wrap">
-              {post.content}
-            </pre>
-
-            {/* Post edit form */}
-            <PostEditForm
-              isEditFormVisible={isEditFormVisible}
-              setIsEditFormVisible={setIsEditFormVisible}
-            />
-
-            {/* Post footer */}
-            <PostFooter
-              postId={postId}
-              commentNo={comments.length}
-              postLikes={postLikes}
-              postDislikes={postDislikes}
-              enableCommentBtn={true}
-              isCommFormVisible={isCommFormVisible}
-              setIsCommFormVisible={setIsCommFormVisible}
-            />
-
-            {/* Comments section */}
-            {post.comments && (
-              <div className="comments-container below-md:relative below-md:right-6 below-md:w-plus-48px">
-                <h3 className="comments-header text-lg pt-1 font-semibold">
-                  Comments:
-                </h3>
-                <div className="comments-list below-md:px-2 bg-gray-100 md:pl-8 md:pr-3 pb-6 mt-1 rounded-md">
-                  {post.comments?.map((commentId) => {
-                    const comment = comments.find((c) => c._id === commentId)
-                    return (
-                      <Comment
-                        key={commentId}
-                        comment={comment}
-                        comments={comments}
-                        setComments={setComments}
-                        commentId={commentId}
-                        depth={0}
-                        postId={postId}
-                        renderChildren={true}
-                      />
-                    )
-                  })}
-                </div>
+        <div className="post-page w-full flex flex-col justify-center md:px-4 mt-1 md:my-8 md:mx-auto">
+          <div className="post-card-container max-w-[800px] mx-auto md:shadow-center-md rounded-md">
+            <div className="post-container relative flex flex-col justify-between w-full pt-4 px-4 md:pb-4">
+              {/* Post header */}
+              <PostHeader author={post.authorData} />
+              <div className="post-header flex justify-between mb-4">
+                <h2 className="post-title text-2xl pt-4 font-semibold">
+                  {post.title}
+                </h2>
               </div>
-            )}
+
+              {/* user infobox on hover */}
+              <Suspense fallback={<UserInfoboxLoader />}>
+                {isUserHovered && (
+                  <LazyUserInfobox
+                    author={post.authorData}
+                    handleMouseEnter={handleMouseEnter}
+                    handleMouseLeave={handleMouseLeave}
+                  />
+                )}
+              </Suspense>
+
+              {/* Post body */}
+              <pre className="post-body-text font-sans whitespace-pre-wrap">
+                {post.content}
+              </pre>
+
+              {/* Post edit form */}
+              <PostEditForm
+                isEditFormVisible={isEditFormVisible}
+                setIsEditFormVisible={setIsEditFormVisible}
+              />
+
+              {/* Post footer */}
+              <PostFooter
+                postId={postId}
+                commentNo={comments.length}
+                postLikes={postLikes}
+                postDislikes={postDislikes}
+                enableCommentBtn={true}
+                isCommFormVisible={isCommFormVisible}
+                setIsCommFormVisible={setIsCommFormVisible}
+              />
+            </div>
+            {/* Comments section */}
+            <div className="post-comments-container md:px-5 md:pb-5 bg-white">
+              {post.comments && (
+                <div className="comments-container below-md:relative">
+                  <h3 className="comments-header text-lg pt-1 below-md:ml-4 font-semibold">
+                    Comments:
+                  </h3>
+                  <div className="comments-list pr-2 pl-7 pb-6 mt-1 bg-gray-100 md:rounded-md">
+                    {post.comments?.map((commentId) => {
+                      const comment = comments.find((c) => c._id === commentId)
+                      return (
+                        <Comment
+                          key={commentId}
+                          comment={comment}
+                          comments={comments}
+                          setComments={setComments}
+                          commentId={commentId}
+                          depth={0}
+                          postId={postId}
+                          renderChildren={true}
+                        />
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </PostContextProvider>
