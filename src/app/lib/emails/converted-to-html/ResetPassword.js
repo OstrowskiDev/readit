3882af102b073a81995dd2fef3728c7f9b15c202
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-13 01:38:20 +0100

    Added jsx and html string templates for reset password mail.

---
+++ src/app/lib/emails/converted-to-html/ResetPassword.js
@@ -0,0 +1,97 @@
+import { env } from 'process'
+
+const urlGitHubRepo = env.URL_GITHUB_PROJECT_REPO
+const urlPortfolio = env.URL_PORTFOLIO
+const urlAppPage = env.URL_READIT_APP
+
+export function generateEmailBody(username, recovery_token) {
+  const urlActivateAccount = `${env.URL_READIT_APP}/account/reset-password?recovery_token=${recovery_token}`
+
+  return `
+      <div style="background-color: #e2e8f0; width: 100%; font-family: ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';">
+        <header style="width: 100%; height: 64px; background-color: #3b82f6;">
+          <table style="height: 100%; role="presentation" cellspacing="0" cellpadding="0" border="0"">
+            <tr>
+              <td align="center">
+                <div style="width: 48px; height: 48px; margin-left: 8px; border-color: white;">
+                  <div style="width: 48px; height: 48px; background-image: url('${urlAppPage}/logo-sm-email-alfa.png'); background-size: cover; background-position: center;"></div>
+                </div>
+              </td>
+              <td align="center">
+                <div style="margin-left: 12px;">
+                  <h1 style="color: white; font-size: 24px; line-height: 32px; font-weight: 600;">ReadIt App</h1>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </header>
+
+        <div style="margin-left: auto; margin-right: auto; margin-top: 64px; margin-bottom: 32px; padding: 40px; width: 480px; height: 560px; background-color: white; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1); border-radius: 8px; box-sizing: border-box;">
+          <h2 style="margin-bottom: 32px; margin-top: 0px; font-weight: 600; font-size: 24px; line-height: 32px; text-align: center; color: black;">
+            Reset Your Password
+          </h2>
+          <p style="margin-top: 56px; color: black; font-size: 16px;">Hello ${username}!</p>
+          <p style="margin-top: 40px; color: black; font-size: 16px; line-height: 24px;">
+            The request was send to change your ReadIt account password. Click
+            button to create new one.
+          </p>
+          <div style="margin-top: 32px; padding: 8px; width: 176px; height: 40px; background-color: #3b82f6; color: white; text-align: center; border-radius: 6px; cursor: pointer; box-sizing: border-box;">
+            <a style="text-decoration: none; color: white; font-weight: bold;  font-size: 16px; line-height: 24px;" href="${urlActivateAccount}" rel="noreferrer noopener" target="_blank">
+              Change Password
+            </a>
+          </div>
+          <p style="margin-top: 32px; color: black; font-size: 16px; line-height: 24px;">
+            You have 15 minutes for password change. If you did not requested to
+            create an account, please ignore this message.
+          </p>
+          <p style="margin-top: 56px; margin-bottom: 0px; color: black; font-size: 16px; line-height: 24px">Best regards,</p>
+          <p style="margin-top: 0px; color: black; font-size: 16px; line-height: 24px;">Ostrowski Dev</p>
+        </div>
+
+        <footer style="width: 100%; margin-bottom: 48px;">
+          <p style="text-align: center; margin-top: 8px; margin-bottom: 8px; color: #6b7280; font-size: 14px; line-height: 20px;">
+            This is an automated message, please do not reply to this email.
+          </p>
+          <div style="text-align: center; margin-top: 16px; margin-bottom: 0px;">
+            <p style="margin: 0px; color: #6b7280; font-size: 14px; line-height: 20px;">
+              Got any problems running this app? Consider opening an issue on
+              <a style="margin-left: 4px; text-decoration: none; color: #6b7280; font-size: 14px; line-height: 20px;" href="${urlGitHubRepo}" target="_blank" rel="noreferrer noopener">
+                GitHub
+              </a>
+              <span style="color: #6b7280;">.</span>
+            </p>
+          </div>
+
+          <div style="width: 480px; padding-bottom: 48px; margin-top: 0px; margin-left: auto; margin-right: auto;">
+            <table style="width: 100%;" role="presentation" cellspacing="0" cellpadding="0" border="0">
+              <tbody style="padding: 0; margin: 0; line-height: 1;">
+                <tr style="padding: 0; margin: 0; line-height: 1;">
+                  <td align="center" style="padding: 0; margin: 0; height: 20px;">
+                    <a style="text-decoration: underline; color: #6b7280; font-size: 14px; line-height: 20px;" href="${urlAppPage}" target="_blank" rel="noreferrer noopener">
+                      ReadIt App Page
+                    </a>
+                  </td>
+                  <td style="padding: 0; margin: 0; height: 20px;">
+                    <p style="color: #6b7280; font-size: 14px; line-height: 20px;">I</p>
+                  </td>
+                  <td align="center" style="padding: 0; margin: 0; height: 20px;">
+                    <a style="text-decoration: underline; color: #6b7280; font-size: 14px; line-height: 20px;" href="${urlPortfolio}" target="_blank" rel="noreferrer noopener">
+                      View My Portfolio
+                    </a>
+                  </td>
+                  <td style="padding: 0; margin: 0; height: 20px;">
+                    <p style="font-size: 14px; color: #6b7280; line-height: 20px;">I</p>
+                  </td>
+                  <td align="center" style="padding: 0; margin: 0; height: 20px;">
+                    <a style="text-decoration: underline; color: #6b7280;" href="${urlGitHubRepo}" target="_blank" rel="noreferrer noopener">
+                      App's GitHub Repo
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </footer>
+      </div>
+  `
+}
---
+++ src/app/lib/emails/previews/ResetPassword.js
@@ -0,0 +1,123 @@
+import { env } from 'process'
+import SmileyFaceIco from '@/app/ui/icons/SmileyFaceIco'
+
+export default function ResetPasswordEmail() {
+  const urlGitHubRepo = env.URL_GITHUB_PROJECT_REPO
+  const urlPortfolio = env.URL_PORTFOLIO
+  const urlAppPage = env.URL_READIT_APP
+  const username = 'John Doe' // change to import in HTML string version
+  const recoveryToken = '1234567890' // change to import in HTML string version
+  const urlActivateAccount = `${env.URL_READIT_APP}/account/reset-password?recovery_token=${recoveryToken}`
+  return (
+    <div className="email-client-mockup my-40 w-[800px] mx-auto  text-black-900">
+      <div className="email-container bg-gray-200 w-full font-sans">
+        <header className="email-header w-full h-16 bg-blue-500">
+          <table className="email-header-table h-full">
+            <tr>
+              <td align="center">
+                <div className="email-header-ico w-12 h-12 ml-2 border-white">
+                  <SmileyFaceIco />
+                </div>
+              </td>
+              <td align="center">
+                <div className="email-header-title ml-3 text-white text-2xl font-semibold">
+                  <h1>ReadIt App</h1>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </header>
+
+        <div className="email-main mx-auto mt-16 mb-8 p-10 w-[480px] h-[560px] bg-white shadow-sm rounded-md">
+          <h2 className="email-main-title mb-8 font-semibold text-2xl text-center ">
+            Reset Your Password
+          </h2>
+          <p className="email-main-greetings mt-14">{`Hello ${username}!`}</p>
+          <p className="email-main-text-1 mt-10">
+            The request was send to change your ReadIt account password. Click
+            button to create new one.{' '}
+          </p>
+          <div className="email-main-activation-btn mt-8 p-2 w-44 h-10 bg-blue-500 text-white text-center font-bold rounded-md hover:cursor-pointer hover:bg-blue-600 active:bg-blue-700 active:shadow-inner active:scale-95">
+            <a
+              className="email-main-activation-anchor underline-none"
+              href={urlActivateAccount}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Change Password
+            </a>
+          </div>
+          <p className="email-main-text-2 mt-8">
+            You have 15 minutes for password change. If you did not requested to
+            create an account, please ignore this message.
+          </p>
+          <p className="email-main-text-3 mt-14"> Best regards, </p>
+          <p className="email-main-text-4"> Ostrowski Dev </p>
+        </div>
+
+        <footer className="email-footer w-full mb-12 text-gray-500 text-sm">
+          <p className="email-footer-text text-center my-2">
+            This is automated message, please do not reply to this email.
+          </p>
+          <div className="email-footer-text-container text-center mt-4">
+            <p className="email-footer-text">
+              Got any problems running this app? Consider opening an issue on
+              <a
+                className="email-footer-link ml-1 underline-none hover:underline hover:text-gray-600"
+                target="_blank"
+                rel="noreferrer noopener"
+                href={urlGitHubRepo}
+              >
+                GitHub
+              </a>
+              <span>.</span>
+            </p>
+          </div>
+
+          <div className="email-footer-socials w-[480px] pb-12 mt-4 mx-auto">
+            <table className="email-footer-socials-table w-full">
+              <tr>
+                <td align="center">
+                  <a
+                    className="email-footer-socials-link underline-none hover:underline hover:text-gray-600"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href={urlAppPage}
+                  >
+                    ReadIt App Page
+                  </a>
+                </td>
+                <td>
+                  <p className="email-footer-socials-separator">I</p>
+                </td>
+                <td align="center">
+                  <a
+                    className="email-footer-socials-link underline-none hover:underline hover:text-gray-600"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href={urlPortfolio}
+                  >
+                    View My Portfolio
+                  </a>
+                </td>
+                <td>
+                  <p className="email-footer-socials-separator">I</p>
+                </td>
+                <td align="center">
+                  <a
+                    className="email-footer-socials-link underline-none hover:underline hover:text-gray-600"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href={urlGitHubRepo}
+                  >
+                    App&apos;s GitHub Repo
+                  </a>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
