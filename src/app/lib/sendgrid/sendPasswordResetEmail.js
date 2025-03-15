8236d8dc6e57da9829f5416992f0373d6b003db9
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-15 23:37:46 +0100

    Connected recover password email template to sendPasswordResetEmail func.

--- src/app/lib/sendgrid/sendPasswordResetEmail.js
+++ src/app/lib/sendgrid/sendPasswordResetEmail.js
@@ -1,26 +1,16 @@
 import sgMail from '@sendgrid/mail'
+import { generateEmailBody } from '../emails/converted-to-html/ResetPassword'
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default function sendPasswordResetEmail(email, recoveryToken, userName) {
   try {
-    const recoveryLink = `http://localhost:3000/account/reset-password?recovery_token=${recoveryToken}`
-
+    const body = generateEmailBody(userName, recoveryToken)
     const msg = {
       to: email,
       from: 'sekretariat@kancelaria-ciesielskamarkiewicz.com.pl', // !!!! use different email in production
       subject: 'ReadIt App - Password Recovery',
-      html: ` 
-      <div class="container">
-        <h2>Password Reset Instructions</h2>
-        <p>Hello <strong>${userName}</strong>,</p>
-        <p>I've received a request to reset your password for your <strong>ReadIt</strong> account. If you made this request, please click the button below to reset your password:</p>
-        <p style="text-align: center;">
-            <a class="button" href="${recoveryLink}" target="_blank">Reset Password</a>
-        </p>
-        <p>This link is valid for the next <strong>60 minutes</strong>. If you did not request a password reset, please ignore this email.</p>
-        <p class="footer">Best regards,<br><strong>OstrowskiDev</strong></p>
-      </div>`,
+      html: body,
     }
     sgMail.send(msg)
   } catch (error) {
