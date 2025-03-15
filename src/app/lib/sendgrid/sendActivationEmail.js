2b3f1dd8009a69b00fa9058877b5d9fcbc2fa3dd
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-15 12:33:36 +0100

    Added proper email body to sendActivationEmail func.

--- src/app/lib/sendgrid/sendActivationEmail.js
+++ src/app/lib/sendgrid/sendActivationEmail.js
@@ -1,19 +1,15 @@
 import sgMail from '@sendgrid/mail'
-
-// !!!! do dopracowania: layout i grafika maila
+import { generateEmailBody } from '@/app/lib/emails/converted-to-html/ConfirmEmail'
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-export async function sendActivationEmail(email, activationToken) {
-  const activationLink = `http://localhost:3000/api/activate-account?activation_token=${activationToken}`
-
+export async function sendActivationEmail(name, email, activationToken) {
+  const body = generateEmailBody(name, activationToken)
   const msg = {
     to: email,
     from: 'sekretariat@kancelaria-ciesielskamarkiewicz.com.pl', // !!!! use different email in production
     subject: 'Activate Your Account on ReadIt App',
-    html: `<p>Click the link below to activate your account:</p>
-           <a href="${activationLink}" target="_blank">Activate Account</a>
-           <p>If you haven't created account on RedIt app ignore this email.</p>`,
+    html: body,
   }
   try {
     await sgMail.send(msg)
