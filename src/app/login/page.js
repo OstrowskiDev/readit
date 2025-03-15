b5fae2f2a8c1f6431f279ce27aaa23ba85fec372
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-15 11:45:23 +0100

    Added error messages to login page UI.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,19 +1,33 @@
 'use client'
 
 import { signIn, useSession } from 'next-auth/react'
+import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import AlreadySignedIn from '../ui/AlreadySignedIn'
 import Link from 'next/link'
+import { ErrorTriangleIco } from '../ui/icons/ErrorTriangleIco'
 
 export default function SignInForm() {
+  const searchParams = useSearchParams()
+  const error = searchParams.get('error')
   const [callbackUrl, setCallbackUrl] = useState(null)
   const { data: session } = useSession()
+
+  const errorMessage = getErrorMessage(error)
+
+  function getErrorMessage(error) {
+    if (!error) return null
+    return error === 'CredentialsSignin'
+      ? 'Invalid email or password. Please try again.'
+      : 'An error occurred. Please try again.'
+  }
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
     const encodedUrl = params.get('callbackUrl')
     let decodedUrl = decodeURIComponent(encodedUrl)
 
+    // !!!! this pattern needs to be change to env var representing the domain
     const httpsPattern = new RegExp('^https://', 'i')
     const httpPattern = new RegExp('^http://', 'i')
 
@@ -52,6 +66,14 @@ export default function SignInForm() {
       <div className="login-container flex flex-col justify-between w-[320px] h-[484px] p-8 rounded-lg bg-blue-500 shadow-lg">
         <h1 className="login-title text-2xl font-semibold text-white">Login</h1>
         <form className="login-form" onSubmit={handleSubmit}>
+          {errorMessage && (
+            <div className="login-error-container flex w-full items-center mb-4 px-4 py-2 rounded-lg text-red-500 text-sm bg-red-100">
+              <div className="login-error-ico w-12">
+                <ErrorTriangleIco color="red" />
+              </div>
+              <p className="login-error-text ml-4">{errorMessage}</p>
+            </div>
+          )}
           <div className="login-email">
             <label
               className="login-email-label text-white block mb-1"
@@ -60,7 +82,11 @@ export default function SignInForm() {
               Email
             </label>
             <input
-              className="login-email-input w-full px-4 py-2 rounded-lg bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className={`login-email-input w-full px-4 py-2 rounded-lg bg-blue-100 focus:bg-white focus:outline-none ring-2 ${
+                errorMessage
+                  ? 'ring-red-400 focus:ring-red-500'
+                  : 'ring-blue-500 focus:ring-blue-400'
+              } `}
               type="email"
               name="email"
               id="email"
@@ -76,7 +102,11 @@ export default function SignInForm() {
               Password
             </label>
             <input
-              className="login-password-input w-full px-4 py-2 rounded-lg bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className={`login-password-input w-full px-4 py-2 rounded-lg bg-blue-100 focus:bg-white focus:outline-none ring-2 ${
+                errorMessage
+                  ? 'ring-red-400 focus:ring-red-500'
+                  : 'ring-blue-500 focus:ring-blue-400'
+              } `}
               type="password"
               name="password"
               id="password"
@@ -91,7 +121,8 @@ export default function SignInForm() {
               </span>
             </Link>
           </div>
-          <div className="login-submit mt-20">
+
+          <div className="login-submit mt-4">
             <button
               className="login-submit-button w-full h-12 bg-white text-blue-500 py-2 rounded-lg hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200 font-bold active:bg-blue-200 hover:text-lg"
               type="submit"
