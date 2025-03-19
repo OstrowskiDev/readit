9c706fdad74d508d506eab9b053e6aaff1a9595d
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-19 01:05:58 +0100

    Added e2e tests for Posts: like button test dislike button test

--- cypress/e2e/posts/posts-crud.cy.js
+++ cypress/e2e/posts/posts-crud.cy.js
@@ -3,9 +3,11 @@ const userEmail = Cypress.env('TEST_USER_EMAIL')
 const userPasswrod = Cypress.env('TEST_USER_PASSWORD')
 const postTitle = 'Cypress Tests Post'
 const postContent =
-  'This is a test post created by Cypress. It will be deleted after the test.'
+  'This is a test post created by Cypress. It will be deleted after the test. Tests include creating, editing, and deleting posts. Also liking and unliking posts.'
+const postTitleEdited = `${postTitle} Edited`
+const postContentEdited = `${postContent} (Edited)`
 
-describe('Posts CRUD + Likes Tests', () => {
+describe('Posts create, update, like, dislike and delete tests.', () => {
   beforeEach(() => {
     cy.login()
   })
@@ -27,12 +29,40 @@ describe('Posts CRUD + Likes Tests', () => {
     })
     cy.url({ timeout: 6000 }).should('include', '/posts/post')
     cy.url().should('include', '?editPost=true')
-    cy.get('.post-title-input').clear().type(`${postTitle} - Edited`)
-    cy.get('.post-content-input').clear().type(`${postContent} - Edited`)
+    cy.get('.post-title-input').clear().type(postTitleEdited)
+    cy.get('.post-content-input').clear().type(postContentEdited)
     cy.get('.post-edit-submit-btn').click()
     cy.get('.toast-text-state').should('be.visible')
     cy.get('.toast-text-message').should('be.visible')
-    cy.get('.post-title').should('contain', `${postTitle} - Edited`)
-    cy.get('.post-content').should('contain', `${postContent} - Edited`)
+    cy.get('.post-title').should('contain', postTitleEdited)
+    cy.get('.post-content').should('contain', postContentEdited)
+  })
+
+  it('should like the post', () => {
+    cy.get('.post-title').each(($el) => {
+      if ($el.text() === postTitleEdited) {
+        cy.wrap($el).parents('.post-container').click()
+      }
+    })
+    cy.url({ timeout: 6000 }).should('include', '/posts/post')
+    cy.get('.post-like-count').should('contain', '0')
+    cy.get('.post-like-button').click()
+    cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-message').should('be.visible')
+    cy.get('.post-like-count').should('contain', '1')
+  })
+
+  it('should dislike the post', () => {
+    cy.get('.post-title').each(($el) => {
+      if ($el.text() === postTitleEdited) {
+        cy.wrap($el).parents('.post-container').click()
+      }
+    })
+    cy.url({ timeout: 6000 }).should('include', '/posts/post')
+    cy.get('.post-like-count').should('contain', '1')
+    cy.get('.post-dislike-button').click()
+    cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-message').should('be.visible')
+    cy.get('.post-like-count').should('contain', '-1')
   })
 })
