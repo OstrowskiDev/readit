f6803a59ccf9d93eee13c105537edadaf2bfda26
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-20 18:50:24 +0100

    Added tests for Posts page delete button.

--- cypress/e2e/posts/posts-crud.cy.js
+++ cypress/e2e/posts/posts-crud.cy.js
@@ -18,7 +18,12 @@ describe('Posts create, update, like, dislike and delete tests.', () => {
     cy.get('.post-content-input').type(postContent)
     cy.get('.post-reply-submit-btn').click()
     cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-state').should('have.text', 'Success!')
     cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Post created successfully!',
+    )
   })
 
   it('should edit the post', () => {
@@ -33,7 +38,12 @@ describe('Posts create, update, like, dislike and delete tests.', () => {
     cy.get('.post-content-input').clear().type(postContentEdited)
     cy.get('.post-edit-submit-btn').click()
     cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-state').should('have.text', 'Success!')
     cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Post updated successfully!',
+    )
     cy.get('.post-title').should('contain', postTitleEdited)
     cy.get('.post-content').should('contain', postContentEdited)
   })
@@ -48,7 +58,12 @@ describe('Posts create, update, like, dislike and delete tests.', () => {
     cy.get('.post-like-count').should('contain', '0')
     cy.get('.post-like-button').click()
     cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-state').should('have.text', 'Success!')
     cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Like added successfully!',
+    )
     cy.get('.post-like-count').should('contain', '1')
   })
 
@@ -62,7 +77,53 @@ describe('Posts create, update, like, dislike and delete tests.', () => {
     cy.get('.post-like-count').should('contain', '1')
     cy.get('.post-dislike-button').click()
     cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-state').should('have.text', 'Success!')
     cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Dislike added successfully!',
+    )
     cy.get('.post-like-count').should('contain', '-1')
+  })
+
+  it('should delete the post via /posts page delete-btn', () => {
+    cy.get('.post-title').each(($el) => {
+      if ($el.text() === postTitleEdited) {
+        cy.wrap($el).parents('.post-container').find('.delete-post-btn').click()
+      }
+    })
+    cy.get('.toast-text-state').should('be.visible')
+    cy.get('.toast-text-state').should('have.text', 'Success!')
+    cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Post deleted successfully!',
+    )
+  })
+})
+
+describe('Delete post via /posts/post page delete-btn', () => {
+  before(() => {
+    cy.visit(`${baseUrl}/posts`)
+    cy.get('.create-new-post-button').click()
+    cy.get('.post-title-input').type(postTitle)
+    cy.get('.post-content-input').type(postContent)
+    cy.get('.post-reply-submit-btn').click()
+    cy.get('.toast-text-state').should('be.visible')
+  })
+  it('should create and delete post', () => {
+    cy.get('.post-title').each(($el) => {
+      if ($el.text() === postTitle) {
+        cy.wrap($el).parents('.post-container').click()
+      }
+    })
+    cy.url({ timeout: 6000 }).should('include', '/posts/post')
+    cy.get('.delete-post-btn').click()
+    cy.get('.toast-text-message').should('be.visible')
+    cy.get('.toast-text-message').should(
+      'have.text',
+      'Post deleted successfully!',
+    )
+    cy.url({ timeout: 6000 }).should('eq', `${baseUrl}/posts`)
   })
 })
