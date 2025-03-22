fd90ec424e24fce8b56cb81f81cd743aa65e2713
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-22 01:06:26 +0100

    Added new variables to e2e mock data. Also unified variable names.

--- cypress/support/mock.js
+++ cypress/support/mock.js
@@ -1,20 +1,24 @@
 const postTitle = 'Cypress Tests Post'
 const postContent =
   'This is a test post created by Cypress. It will be deleted after the test. Tests include creating, editing, deleting and commenting top-level comments. Also adding and removing likes from top-level comments.'
+const postTitleEdited = `${postTitle} Edited`
+const postContentEdited = `${postContent} (Edited)`
 const commentContent =
   'This is a test top-level comment created by Cypress. It will be deleted after the test. Tests include creating, editing, deleting and commenting top-level comments. Also adding and removing likes from top-level comments.'
-const newCommentContent = `${commentContent} Edited`
+const commentContentEdited = `${commentContent} Edited`
 const replyContent =
   'This is a test reply-level comment created by Cypress. It will be deleted after the test. Tests include creating, editing, deleting and commenting reply-level comments. Also adding and removing likes from reply-level comments.'
-const newReplyContent = `${replyContent} Edited`
+const replyContentEdited = `${replyContent} Edited`
 
 const mockData = {
   postTitle,
+  postTitleEdited,
   postContent,
+  postContentEdited,
   commentContent,
-  newCommentContent,
+  commentContentEdited,
   replyContent,
-  newReplyContent,
+  replyContentEdited,
 }
 
 export default mockData
