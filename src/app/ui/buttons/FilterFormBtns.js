1f11d5855f5f4717f5a40e0c61c5ef5befb13a1b
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-23 10:55:14 +0100

    Added E2E tests for Search Bar and Filter Options: - sort by popularity in ascending/descending order tests - sort  by activity in ascending/descending order tests

--- src/app/ui/buttons/FilterFormBtns.js
+++ src/app/ui/buttons/FilterFormBtns.js
@@ -8,16 +8,16 @@ export function FilterFormBtns({
   }
 
   return (
-    <div className="post-reply-btns flex justify-end">
+    <div className="filter-options-btns flex justify-end">
       <button
-        className="post-reply-cancel-btn btn-gray py-1 px-2 mt-1"
+        className="filter-options-cancel-btn btn-gray py-1 px-2 mt-1"
         type="button"
         onClick={onCancelClick}
       >
         Cancel
       </button>
       <button
-        className="post-reply-submit-btn btn-blue py-1 px-2 mx-2 mt-1"
+        className="filter-options-submit-btn btn-blue py-1 px-2 mx-2 mt-1"
         type="button"
         onClick={onSubmit}
       >
