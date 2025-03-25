ae2d97663a2016a919db716ffc44d9ae252d816a
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-25 20:25:14 +0100

    Added more strict test rules to validateSignUp tests.

--- __tests__/unit/validateSignUp.test.js
+++ __tests__/unit/validateSignUp.test.js
@@ -10,11 +10,66 @@ describe('validateSignUp function', () => {
     expect(result.password.message).toContain('This field is required.')
   })
 
-  test('should validate correct email format', () => {
+  test('should require proper email format', () => {
     const formData = {
-      name: 'John Doe',
+      name: 'Johnny',
       email: 'invalid-email',
-      password: 'Abc123!',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.email.message).toContain('Please enter valid email address.')
+  })
+
+  test('should not allow dot at the begginign of email address', () => {
+    const formData = {
+      name: 'Johnny',
+      email: '.invalid@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.email.message).toContain('Please enter valid email address.')
+  })
+
+  test('should not allow dot before @ in email address', () => {
+    const formData = {
+      name: 'Johnny',
+      email: '.invalid@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.email.message).toContain('Please enter valid email address.')
+  })
+
+  test('should not allow dot two or more @ in email address', () => {
+    const formData = {
+      name: 'Johnny',
+      email: 'invalid@this@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.email.message).toContain('Please enter valid email address.')
+  })
+
+  test('should not allow - in front of domain in email address', () => {
+    const formData = {
+      name: 'Johnny',
+      email: 'invalid@-example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.email.message).toContain('Please enter valid email address.')
+  })
+
+  test('should not allow - at the end of domain in email address', () => {
+    const formData = {
+      name: 'Johnny',
+      email: 'invalid@example-.com',
+      password: 'pass123',
     }
     const result = validateSignUp(formData)
 
@@ -23,20 +78,109 @@ describe('validateSignUp function', () => {
 
   test('should allow correct email format', () => {
     const formData = {
-      name: 'John Doe',
+      name: 'Johnny',
       email: 'test@example.com',
-      password: 'Abc123!',
+      password: 'pass123',
     }
     const result = validateSignUp(formData)
 
     expect(result.email.message).toHaveLength(0)
   })
 
-  test('should validate username characters', () => {
+  test('should not allow username with numbers', () => {
     const formData = {
       name: 'John123',
       email: 'test@example.com',
-      password: 'Abc123!',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should not allow username with special characters', () => {
+    const formData = {
+      name: 'Joh@nn@',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should allow using space inside username', () => {
+    const formData = {
+      name: 'John Frank',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toHaveLength(0)
+  })
+
+  test('should not allow username that starts with space character', () => {
+    const formData = {
+      name: ' John Frank',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should not allow username that ends with space character', () => {
+    const formData = {
+      name: 'John Frank ',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should not allow username that uses "admin" word', () => {
+    const formData = {
+      name: 'your admin',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should not allow username that uses "moderator" word', () => {
+    const formData = {
+      name: 'main moderator',
+      email: 'test@example.com',
+      password: 'pass123',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.name.message).toContain(
+      'Name cannot contain numbers or special characters.',
+    )
+  })
+
+  test('should not allow username that uses "root" word', () => {
+    const formData = {
+      name: 'sysroot',
+      email: 'test@example.com',
+      password: 'pass123',
     }
     const result = validateSignUp(formData)
 
@@ -49,7 +193,7 @@ describe('validateSignUp function', () => {
     const formData = {
       name: 'ThisIsAVeryLongNameExceedingLimit',
       email: 'test@example.com',
-      password: 'Abc123!',
+      password: 'pass123',
     }
     const result = validateSignUp(formData)
 
@@ -58,11 +202,24 @@ describe('validateSignUp function', () => {
     )
   })
 
+  test('should validate password length', () => {
+    const formData = {
+      name: 'Johnny',
+      email: 'test@example.com',
+      password: 'Short!1',
+    }
+    const result = validateSignUp(formData)
+
+    expect(result.password.message).toContain(
+      'Password must be at least 8 characters, with an uppercase letter, lowercase letter, number, and special character.',
+    )
+  })
+
   test('should validate password strength', () => {
     const formData = {
-      name: 'John Doe',
+      name: 'Johnny',
       email: 'test@example.com',
-      password: 'weakpass',
+      password: 'weak!1',
     }
     const result = validateSignUp(formData)
 
@@ -73,7 +230,7 @@ describe('validateSignUp function', () => {
 
   test('should allow a strong password', () => {
     const formData = {
-      name: 'John Doe',
+      name: 'Johnny',
       email: 'test@example.com',
       password: 'StrongPass1!',
     }
