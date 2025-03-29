51bf8c8a24f7bfab0551a7f4f1929967d26e6ae3
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-03-29 15:34:07 +0100

    Moved dev test-email route to /tests/test-email. Added logic that will redirect to 404 when in production.

--- src/app/test-email/page.js
+++
@@ -1,6 +0,0 @@
-import ResetPasswordEmail from '../lib/emails/previews/ResetPassword'
-// !!!! delete this page so its not used in production
-
-export default function MailTemplateConfirmEmail() {
-  return <ResetPasswordEmail />
-}
---
+++ src/app/tests/test-email/page.js
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import ResetPasswordEmail from '../../lib/emails/previews/ResetPassword'
+
+export default function MailTemplateConfirmEmail() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      router.push('/404')
+    }
+  }, [])
+
+  if (process.env.NODE_ENV === 'production') {
+    return null
+  }
+  return <ResetPasswordEmail />
+}
