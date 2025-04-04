c975eaeb0e8cb36f3a2945edff59bca6f1c2eca9
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-04 22:52:03 +0200

    Added imageTypesWhitelist for MIME veryfication.

---
+++ src/app/lib/security/imageTypesWhitelist.js
@@ -0,0 +1,8 @@
+const imageTypesWhitelist = [
+  { ext: 'png', mime: 'image/png' },
+  { ext: 'jpg', mime: 'image/jpeg' },
+  { ext: 'jpeg', mime: 'image/jpeg' },
+  { ext: 'gif', mime: 'image/gif' },
+  { ext: 'webp', mime: 'image/webp' },
+  { ext: 'bmp', mime: 'image/bmp' },
+]
