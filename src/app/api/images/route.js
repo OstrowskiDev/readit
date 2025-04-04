c60e297f377e370dc2781a287948f02d517662c6
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-04 20:37:01 +0200

    Integrated all image methods to one RESTful api route.

---
+++ src/app/api/images/route.js
@@ -0,0 +1,95 @@
+import s3 from '@/app/lib/cloudflare-sdk/s3'
+import { NextResponse } from 'next/server'
+
+export async function GET(req, res) {
+  console.log('image get API route was triggered...')
+  // const { key } = req.query;
+  const key = 'testfile.png'
+  try {
+    const data = await s3
+      .getObject({
+        Bucket: process.env.CLOUDFLARE_BUCKET,
+        Key: key,
+      })
+      .promise()
+
+    return new NextResponse(data.Body, {
+      status: 200,
+      headers: {
+        'Content-Type': data.ContentType,
+      },
+    })
+  } catch (error) {
+    console.error('unexpected error in image get API route', error)
+    return NextResponse.json(
+      { error: 'Error retrieving file' },
+      { status: 500 },
+    )
+  }
+}
+
+export async function PUT(req) {
+  try {
+    const formData = await req.formData()
+    const file = formData.get('file')
+    const _id = formData.get('_id')
+
+    if (!file || !_id) {
+      return NextResponse.json(
+        { error: 'Missing file or _id' },
+        { status: 400 },
+      )
+    }
+
+    const fileBuffer = await file.arrayBuffer()
+    const fileName = `${_id}.png`
+
+    await s3
+      .putObject({
+        Bucket: process.env.CLOUDFLARE_BUCKET,
+        Key: fileName,
+        Body: Buffer.from(fileBuffer),
+        ContentType: 'image/png',
+      })
+      .promise()
+
+    return NextResponse.json(
+      { message: 'File uploaded successfully', fileName },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error('Unexpected error in image upload API route', error)
+    return NextResponse.json({ error: 'Error uploading file' }, { status: 500 })
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const _id = searchParams.get('_id')
+
+    if (!_id) {
+      return NextResponse.json(
+        { error: 'Missing _id parameter' },
+        { status: 400 },
+      )
+    }
+
+    const fileName = `${_id}.png`
+
+    await s3
+      .deleteObject({
+        Bucket: process.env.CLOUDFLARE_BUCKET,
+        Key: fileName,
+      })
+      .promise()
+
+    return NextResponse.json(
+      { message: 'File deleted successfully', fileName },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error('Unexpected error in image delete API route', error)
+    return NextResponse.json({ error: 'Error deleting file' }, { status: 500 })
+  }
+}
