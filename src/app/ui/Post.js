f185abb7040c51d773aa4a16fd1bc2ff51dd1731
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-15 01:42:38 +0200

    Added handling images of different aspect raito (blur effect) to post cards in main Posts page.

--- src/app/ui/Post.js
+++ src/app/ui/Post.js
@@ -60,7 +60,7 @@ export function Post({
 
               {/* Post body */}
               {hasImage ? (
-                <div className="post-image-container aspect-[16/9] w-full bg-gray-200">
+                <div className="post-image-container relative aspect-[16/9] w-full overflow-hidden bg-gray-200">
                   {isLoading && (
                     <div className="post-image-container flex justify-center  aspect-[16/9] w-full bg-gray-200 rounded-md relative">
                       <ImageShimmerAnimated />
@@ -68,11 +68,18 @@ export function Post({
                   )}
 
                   <img
-                    className="post-image"
+                    className="post-image relative z-10  h-full w-full object-contain"
                     src={`/api/images/${postId}.${imageExtension}`}
                     alt="post image"
                     onLoad={() => setIsLoading(false)}
                     onError={() => setIsLoading(false)}
+                  />
+
+                  <img
+                    src={`/api/images/${postId}.${imageExtension}`}
+                    alt=""
+                    aria-hidden="true"
+                    className="post-image-blur absolute inset-0 h-full w-full object-cover scale-110 blur-md brightness-75"
                   />
                 </div>
               ) : (
