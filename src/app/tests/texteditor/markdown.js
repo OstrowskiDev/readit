ad8c5810163f1b1acad90352c03ce901f9a94e64
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-18 19:48:57 +0200

    Added initial markdown with edit styles for tests.

---
+++ src/app/tests/texteditor/markdown.js
@@ -0,0 +1,36 @@
+const testMarkdown = `
+# Nagłówek 1
+
+## Nagłówek 2
+
+**Pogrubienie**  
+*Kursywa*  
+~~Przekreślenie~~  
+__Podkreślenie__ (nieobsługiwane natywnie w Markdown)  
+\`Kod inline\`
+
+[Link](https://example.com)
+
+---
+
+**Lista wypunktowana:**
+- Element listy 1
+- Element listy 2
+  - Zagnieżdżony element
+
+**Lista numerowana:**
+1. Element 1
+2. Element 2
+
+> Cytat blokowy
+
+\`\`\`js
+console.log('Kod blokowy');
+\`\`\`
+
+| Kolumna 1 | Kolumna 2 |
+|-----------|-----------|
+| A         | B         |
+`
+
+export default testMarkdown
