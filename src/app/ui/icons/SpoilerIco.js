ca338dfe65118b10d80508b9d623b9eebc3bd241
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-28 21:18:32 +0200

    Fixed SpoilerIco svg props not being properly translated to JSX.

--- src/app/ui/icons/SpoilerIco.js
+++ src/app/ui/icons/SpoilerIco.js
@@ -5,9 +5,9 @@ export default function SpoilerIco() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M12 16h.01" />
       <path d="M12 8v4" />
