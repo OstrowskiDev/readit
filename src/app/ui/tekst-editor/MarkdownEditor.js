1b5b6d92a47137db18bb03f4e858ad3fce8f0e3a
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-04-29 19:16:23 +0200

    Added UI elements to MarkdonwEditor.

--- src/app/ui/tekst-editor/MarkdownEditor.js
+++ src/app/ui/tekst-editor/MarkdownEditor.js
@@ -1,12 +1,18 @@
 'use client'
 
-import { useTextEnditorContext } from '@/app/lib/context/TextEditorProvider'
-import parseHtmlToMarkdown from '@/app/lib/text-editor/parseHtmlToMarkdown'
+import { useTextEditorContext } from '@/app/lib/context/TextEditorProvider'
+import { parseHtmlToMarkdown } from '@/app/lib/text-editor/parseHtmlToMarkdown'
 import { useEffect } from 'react'
+import { ToggleEditorTypeBtn } from './ToggleEditorTypeBtn'
 
 export function MarkdownEditor() {
+  //!!!! add onInputChange to context import and delete onInputChange func below
   const { htmlString, markdownString, setMarkdownString } =
-    useTextEnditorContext()
+    useTextEditorContext()
+
+  function onInputChange(event) {
+    setMarkdownString(event.target.value)
+  }
 
   useEffect(() => {
     const newMarkdown = parseHtmlToMarkdown(htmlString)
@@ -14,16 +20,21 @@ export function MarkdownEditor() {
   }, [])
 
   return (
-    <div className="markdown-editor-container">
-      <input
-        className="markdown-editor-input"
-        type="text"
-        name="markdownString"
-        id="markdownString"
-        value={markdownString}
-        onChange={onInputChange}
-        required
-      ></input>
+    <div className="markdown-editor">
+      <div className="markdown-editor-container px-4 py-3 border border-gray-400 rounded-md">
+        <div className="markdown-editor-btns-container flex w-full">
+          <ToggleEditorTypeBtn />
+        </div>
+        <textarea
+          className="markdown-editor-input w-full h-32 px-3 py-2 border-none focus:outline-none"
+          type="text"
+          name="markdownString"
+          id="markdownString"
+          value={markdownString}
+          onChange={onInputChange}
+          required
+        ></textarea>
+      </div>
     </div>
   )
 }
