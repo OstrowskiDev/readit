c1aef7c9e5b7dc2a5f0ec4dd50570bafe4373371
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-05-17 17:29:10 +0200

    Changed getAuthCookies by adding logic that handles next auth production sufixes for its cookies. Now getAuthCookies will return proper values both in prod and in dev.

--- src/lib/security/getAuthCookies.js
+++ src/lib/security/getAuthCookies.js
@@ -1,13 +1,36 @@
 export function getAuthCookies(cookies) {
-  const sessionToken = cookies.get('next-auth.session-token')
-  const csrfToken = cookies.get('next-auth.csrf-token')
-  const callbackUrl = cookies.get('next-auth.callback-url')
+  let sessionToken
+  let csrfToken
+  let callbackUrl
 
-  const cookieHeader = [
-    `next-auth.session-token=${sessionToken.value}`,
-    `next-auth.csrf-token=${csrfToken.value}`,
-    `next-auth.callback-url=${callbackUrl.value}`,
-  ].join('; ')
+  const nodeEnv = process.env.NODE_ENV
+
+  if (nodeEnv === 'production') {
+    sessionToken = cookies.get('__Secure-next-auth.session-token')
+    csrfToken = cookies.get('__Host-next-auth.csrf-token')
+    callbackUrl = cookies.get('__Secure-next-auth.callback-url')
+  } else {
+    sessionToken = cookies.get('next-auth.session-token')
+    csrfToken = cookies.get('next-auth.csrf-token')
+    callbackUrl = cookies.get('next-auth.callback-url')
+  }
+
+  if (!sessionToken || !csrfToken || !callbackUrl) {
+    throw new Error('Missing required auth cookies')
+  }
+
+  const cookieHeader =
+    nodeEnv === 'production'
+      ? [
+          `__Secure-next-auth.session-token=${sessionToken.value}`,
+          `__Host-next-auth.csrf-token=${csrfToken.value}`,
+          `__Secure-next-auth.callback-url=${callbackUrl.value}`,
+        ].join('; ')
+      : [
+          `next-auth.session-token=${sessionToken.value}`,
+          `next-auth.csrf-token=${csrfToken.value}`,
+          `next-auth.callback-url=${callbackUrl.value}`,
+        ].join('; ')
 
   return cookieHeader
 }
