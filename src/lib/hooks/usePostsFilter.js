1061142a523ce80f52ccf82246d8dcdce43e6739
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-05-26 14:26:50 +0200

    Expanded and updated usePostsFilter hook. Now it can handle different actions: applyFastQuery, applyFilters, applyPagination, applyPageLimit.

--- src/lib/hooks/usePostsFilter.js
+++ src/lib/hooks/usePostsFilter.js
@@ -11,30 +11,78 @@ export function usePostsFilter({
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  async function getFilteredPosts(filterData) {
+  async function applyFastQuery(queryString) {
     const params = new URLSearchParams(searchParams)
-    for (const key in filterData) {
-      if (filterData[key]) {
-        console.log(key, filterData[key])
-        params.set(key, filterData[key])
-      }
-    }
-    if (filterData.title && params.get('fastQuery')) {
+
+    if (queryString.trim()) {
+      params.set('fastQuery', queryString.trim())
+    } else {
       params.delete('fastQuery')
     }
-    replace(`${pathname}?${params.toString()}`)
+
+    params.delete('title')
+    params.delete('content')
+    params.delete('author')
+    params.delete('sortBy')
+    params.delete('sortOrder')
+
+    params.set('page', '1')
 
     if (onlyCurrentUserPosts) {
-      params.set(onlyCurrentUserPosts, onlyCurrentUserPosts)
+      params.set('onlyCurrentUserPosts', onlyCurrentUserPosts)
     }
 
-    const filterParams = Object.fromEntries(params.entries())
+    replace(`${pathname}?${params.toString()}`)
+    await fetchAndSetPosts(params)
+  }
 
+  async function applyFilters(
+    filters = { sortBy: 'time', order: 'descending' },
+  ) {
+    const params = new URLSearchParams()
+
+    for (const key in filters) {
+      if (filters[key]) {
+        params.set(key, filters[key])
+      }
+    }
+
+    params.delete('fastQuery')
+    params.set('page', '1')
+
+    if (onlyCurrentUserPosts) {
+      params.set('onlyCurrentUserPosts', onlyCurrentUserPosts)
+    }
+
+    replace(`${pathname}?${params.toString()}`)
+    await fetchAndSetPosts(params)
+  }
+
+  async function applyPagination(pageNum) {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', pageNum)
+
+    replace(`${pathname}?${params.toString()}`)
+    await fetchAndSetPosts(params)
+  }
+
+  async function applyPageLimit(limit = 10) {
+    const params = new URLSearchParams(searchParams)
+    params.set('limit', limit)
+    params.set('page', '1')
+
+    replace(`${pathname}?${params.toString()}`)
+    await fetchAndSetPosts(params)
+  }
+
+  async function fetchAndSetPosts(params) {
+    const filterParams = Object.fromEntries(params.entries())
     const postsData = await filterPosts(filterParams)
     setPosts(postsData.posts)
     setPostsCount(postsData.postsCount)
-    setTriggerReset((prev) => !prev)
+    // !!!! check if this is still needed
+    // setTriggerReset((prev) => !prev)
   }
 
-  return { getFilteredPosts }
+  return { applyFastQuery, applyFilters, applyPagination, applyPageLimit }
 }
