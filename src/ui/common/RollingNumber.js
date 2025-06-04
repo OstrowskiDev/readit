43d86505dccf6f8c04e69e961405a21cc8b129c5
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-06-05 01:56:25 +0200

    Added negative number (minus sign) handling to RollingNumber component.

--- src/ui/common/RollingNumber.js
+++ src/ui/common/RollingNumber.js
@@ -1,18 +1,62 @@
-import React from 'react'
-
 const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 function getTranslateY(digit) {
-  if (digit === '-') return '-100%'
   return `-${parseInt(digit, 10) * 10}%`
 }
 
 export function RollingNumber({ value = 0, duration = 1000, lineHeight = 26 }) {
-  const digits = typeof value === 'number' ? value.toString().split('') : ['0']
+  const isNegative = typeof value === 'number' && value < 0
+  // create array with non-negative (absolute) number strings
+  // minus sign (-) will be handled separately, so its not included
+  console.log(value)
+  console.log(typeof value)
+  const absoluteDigits =
+    typeof value === 'number' ? Math.abs(value).toString().split('') : ['0']
 
   return (
     <span style={{ display: 'inline-flex' }}>
-      {digits.map((digit, idx) => (
+      {/* handle minus (-) sign */}
+      {isNegative && (
+        <span
+          className="digit minus"
+          style={{
+            lineHeight: `${lineHeight}px`,
+            width: '1ch',
+            overflow: 'hidden',
+            display: 'inline-flex',
+            position: 'relative',
+          }}
+        >
+          <span
+            className="scale"
+            aria-hidden="true"
+            style={{
+              userSelect: 'none',
+              position: 'absolute',
+              left: 0,
+              display: 'inline-flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              transition: `transform ${duration}ms`,
+              transform: 'translateY(0%)',
+            }}
+          >
+            <span>-</span>
+          </span>
+          <span
+            className="value"
+            style={{
+              color: 'transparent',
+              position: 'relative',
+            }}
+          >
+            -
+          </span>
+        </span>
+      )}
+      {/* handle number strings */}
+      {absoluteDigits.map((digit, idx) => (
         <span
           key={idx}
           className="digit"
@@ -39,6 +83,7 @@ export function RollingNumber({ value = 0, duration = 1000, lineHeight = 26 }) {
               transform: `translateY(${getTranslateY(digit)})`,
             }}
           >
+            {/* render all possible digits as initially transparent */}
             {DIGITS.map((d) => (
               <span key={d}>{d}</span>
             ))}
