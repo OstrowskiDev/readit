8b868acf838051b9cc516c3b948639233cd8dedc
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-06-04 17:39:55 +0200

    Revamped user profiles page. Added button wrapper to Filter, preventing meging its color with sites bg image.

--- src/app/tests/shimmer/page.js
+++
@@ -1,5 +0,0 @@
-import { PostShimmer } from '@/ui/loaders/PostShimmer'
-
-export default function ShimmerTests() {
-  return <PostShimmer />
-}
--- src/ui/buttons/FilterBtn.js
+++ src/ui/buttons/FilterBtn.js
@@ -4,11 +4,13 @@ export function FilterBtn({ isFilterFormVis, setIsFilterFormVis }) {
   }
 
   return (
-    <button
-      onClick={onFilterClick}
-      className="filter-btn button-orange-strong below-xs:grow h-10 px-4 md:ml-2 ml-0"
-    >
-      Filter
-    </button>
+    <div className="wrapper-orange-btn-bg md:ml-2 ml-0">
+      <button
+        onClick={onFilterClick}
+        className="filter-btn button-orange-strong below-xs:grow h-10 px-4 "
+      >
+        Filter
+      </button>
+    </div>
   )
 }
