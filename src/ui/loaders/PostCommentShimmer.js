c15bacda3244f24e74d084ffeab5fc98bf94ebf1
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-10-03 20:37:21 +0200

    Refactored PostCommentShimmer: more componentization and better fit to current UI layout.

--- src/ui/loaders/PostCommentShimmer.js
+++ src/ui/loaders/PostCommentShimmer.js
@@ -1,64 +1,39 @@
 import './shimmerStyles.css'
 import { CommentShimmer } from './CommentShimmer'
+import ShimmerParagraph from './common/ShimmerParagraph'
+import ShimmerHeader from './common/ShimmerHeader'
 
 export function PostCommentShimmer({ hasImg }) {
   return (
     <div className="post-shimmer-container relative w-full">
-      <div className="post-shimmer shimmer-border relative max-w-[800px] py-1 px-4 mx-auto glass-blue-soft rounded-xl ">
+      <div className="post-shimmer shimmer-border relative max-w-[800px] py-1 px-4 mx-auto glass-blue-soft rounded-md ">
         {/* Post header */}
         <div className="post-shimmer-header relative right-0 flex items-center mt-3">
           <div className="shimmer avatar absolute h-12 w-12 rounded-full"></div>
-          <div className="shimmer text-authors-name ml-2 h-4 w-24"></div>
-          <div className="shimmer text-creation-date h-4 w-20 ml-2"></div>
-          <div className="shimmer text-edit-date h-4 w-28 ml-2"></div>
+          <ShimmerParagraph width="120px" height="16px" classes="ml-2" />
+          <ShimmerParagraph width="80px" height="16px" classes="ml-2" />
+          <ShimmerParagraph width="100px" height="16px" classes="ml-2" />
         </div>
 
         {/* Post title */}
-        <div className="shimmer text-post-title h-5 w-96 mt-6 mb-2"></div>
+        <div className="shimmer text-post-title h-6 w-[420px] mt-5 mb-5"></div>
 
         {hasImg && (
           <div className="shimmer aspect-[16/9] h-full w-full overflow-hidden rounded-md z-0"></div>
         )}
 
         {/* Post body */}
-        <div className="post-content mt-10 mb-3">
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: '100%' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 34px)' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 94px)' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 14px)' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 164px)' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 284px)' }}
-          ></div>
-
-          <div
-            className="shimmer text h-3 mb-3 mt-8"
-            style={{ width: '100%' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 34px)' }}
-          ></div>
-          <div
-            className="shimmer text h-3 mb-3"
-            style={{ width: 'calc(100% - 94px)' }}
-          ></div>
+        <div className="post-content mt-5 mb-3">
+          <ShimmerHeader width="200px" classes="mb-5" />
+          <ShimmerParagraph width="100%" classes="mb-3" />
+          <ShimmerParagraph width="calc(100% - 34px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="calc(100% - 94px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="calc(100% - 14px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="calc(100% - 164px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="calc(100% - 284px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="100%" classes="mb-3" />
+          <ShimmerParagraph width="calc(100% - 34px)" classes="mb-3 h-3" />
+          <ShimmerParagraph width="calc(100% - 94px)" classes="mb-3 h-3" />
         </div>
 
         {/* Post footer */}
